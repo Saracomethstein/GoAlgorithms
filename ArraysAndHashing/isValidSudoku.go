5c39db4bde87db8e6f9005d2d06fcc065b898f49
Adel Sevastianov <adelsevastanov@gmail.com>
2024-08-20 11:53:05 +0300

    add: validSudoku

---
+++ ArraysAndHashing/isValidSudoku.go
@@ -0,0 +1,27 @@
+package arrays_and_hashing
+
+func IsValidSudoku(board [][]byte) bool {
+	mapBoard := make(map[string]bool)
+	for i := range board {
+		for j := range board {
+			val := string(board[i][j])
+
+			if val == "." {
+				continue
+			}
+
+			_, ok1 := mapBoard[val+"row"+string(i)]
+			_, ok2 := mapBoard[val+"column"+string(j)]
+			_, ok3 := mapBoard[val+"grid"+string(i/3)+"-"+string(j/3)]
+
+			if ok1 || ok2 || ok3 {
+				return false
+			} else {
+				mapBoard[val+"row"+string(i)] = true
+				mapBoard[val+"column"+string(j)] = true
+				mapBoard[val+"grid"+string(i/3)+"-"+string(j/3)] = true
+			}
+		}
+	}
+	return true
+}
