3704fce7e23da0ea763c44a3c2c0b049afc87bb4
barathea <barathea@student.21-school.ru>
2024-08-22 23:08:55 +0300

    up: link to task

--- ArraysAndHashing/twoSum.go
+++ ArraysAndHashing/twoSum.go
@@ -1,5 +1,8 @@
 package arrays_and_hashing
 
+// link to the task on leetcode 
+// https://leetcode.com/problems/two-sum/description/
+
 func TwoSum(nums []int, target int) []int {
 	var hash = make(map[int]int)
 	for i, num := range nums {
