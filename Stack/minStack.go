212a46ef0585cbd97992b02d833bf30169409508
Adel Sevastianov <adelsevastanov@gmail.com>
2024-08-25 16:19:00 +0300

    up: solv

--- Stack/minStack.go
+++ Stack/minStack.go
@@ -1,23 +1,54 @@
 package stack
 
+// // link to the task on leetcode
+// https://leetcode.com/problems/min-stack/submissions/1367892687/
+
 type MinStack struct {
+	min int
+	top *StackNode
 }
 
+type StackNode struct {
+	data    int
+	lastMin int
+	next    *StackNode
+}
+
+var newTop *StackNode
+
 func Constructor() MinStack {
+	return MinStack{top: nil, min: 0}
 }
 
 func (this *MinStack) Push(val int) {
+	if this.top == nil {
+		newTop = &StackNode{data: val, next: this.top}
+		this.min = val
+	} else {
+		newTop = &StackNode{data: val, lastMin: this.min, next: this.top}
+	}
 
+	this.top = newTop
+
+	if this.top.data < this.min {
+		this.min = this.top.data
+	}
 }
 
 func (this *MinStack) Pop() {
+	if this.top.next == nil {
+		this.top = nil
+		return
+	}
 
+	this.min = this.top.lastMin
+	*this.top = *this.top.next
 }
 
 func (this *MinStack) Top() int {
-
+	return this.top.data
 }
 
 func (this *MinStack) GetMin() int {
-
+	return this.min
 }
