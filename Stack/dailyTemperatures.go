6d7c8e8a0e8bf79b52ff20ffb9d8cab487439190
Adel Sevastianov <adelsevastanov@gmail.com>
2024-08-26 13:09:37 +0300

    up: new comment

--- Stack/dailyTemperatures.go
+++ Stack/dailyTemperatures.go
@@ -1,5 +1,8 @@
 package stack
 
+// link to the task on leetcode
+// https://leetcode.com/problems/daily-temperatures/submissions/1368818322/
+
 func DailyTemperatures(temperatures []int) []int {
 	answer := make([]int, len(temperatures))
 
