385ef0bef8ec07fe6ff8b6b3ba196b0128f460fc
Adel Sevastianov <adelsevastanov@gmail.com>
2024-08-28 16:41:17 +0300

    add: new solv

---
+++ ArraysAndHashing/mergeSortedArray.go
@@ -0,0 +1,17 @@
+package arrays_and_hashing
+
+import (
+	"sort"
+)
+
+// link to the task on leetcode
+// https://leetcode.com/problems/merge-sorted-array/description/
+
+func Merge(nums1 []int, m int, nums2 []int, n int) {
+	j := 0
+	for i := m; i < m+n; i++ {
+		nums1[i] = nums2[j]
+		j++
+	}
+	sort.Ints(nums1)
+}
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	stack "GoAlgorithms/Stack"
+	arrays_and_hashing "GoAlgorithms/ArraysAndHashing"
 	"fmt"
 )
 
 func main() {
-	height := []int{2, 1, 5, 6, 2, 3}
-	fmt.Println("Answer:", stack.LargestRectangleArea(height))
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+	arrays_and_hashing.Merge(nums1, 3, nums2, 3)
+	fmt.Println(nums1)
 }
